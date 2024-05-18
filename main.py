c741fee16e61044cddb9665de8382a4966dcd299
Oskar <o.wrobel2019@gmail.com>
2024-05-18 09:28:43 +0200

    Cleaned up. Added OOP

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import sys
 
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
 
-pygame.display.set_caption('Platformer')
-screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption('Platformer')
+        self.screen = pygame.display.set_mode((1280, 720))
 
-clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-    pygame.display.update()
-    clock.tick(60)
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
+
+
