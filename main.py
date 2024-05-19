591bb35151ee367a7158c8c43a9af35387bdc004
Oskar <o.wrobel2019@gmail.com>
2024-05-19 17:53:11 +0200

    Added basic entity and screen rendering. Changed player sprites.

--- main.py
+++ main.py
@@ -2,34 +2,36 @@ import sys
 
 import pygame
 
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image
+
 
 class Game:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption('Platformer')
-        self.screen = pygame.display.set_mode((1280, 720))
+
+        self.screen = pygame.display.set_mode((1280, 720))  # Window surface
+        self.display = pygame.Surface((640, 320))  # Render surface, scaled down 2x (for 4x: 320, 180)
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.img_pos = [640, 360]
         self.movement = [False, False]
 
-        self.collision_area = pygame.Rect(640, 360, 360, 60)
+        # asset dictionary
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (100, 100), (6, 16))
 
     def run(self):
         while True:
-            self.screen.fill((14, 180, 245))
+            self.display.fill((14, 180, 245))
 
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 255, 100), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 100, 100), self.collision_area)
-
-            self.img_pos[1] += 5 * (self.movement[1] - self.movement[0])
-            self.screen.blit(self.img, self.img_pos)
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -37,17 +39,18 @@ class Game:
                     sys.exit()
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
---
+++ scripts/utils.py
@@ -0,0 +1,10 @@
+import pygame
+
+BASE_IMG_PATH = 'data/images/'
+
+
+def load_image(path):
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img.set_colorkey((0, 0, 0))
+    # 'img.set_colorkey((0, 0, 0))' if sprites have a black background
+    return img
