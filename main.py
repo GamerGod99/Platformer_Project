056acdb16ae499064ec9673800e636426282b058
Oskar <o.wrobel2019@gmail.com>
2024-05-25 10:19:48 +0200

    Added jumping mechanic

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class Game:
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
 
-            print(self.tilemap.physics_rects_around(self.player.pos))
+            # print(self.tilemap.physics_rects_around(self.player.pos))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -53,6 +53,9 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
